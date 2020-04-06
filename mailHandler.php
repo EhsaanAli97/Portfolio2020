3f048ec9f6301aa6357f49a6ed2206eae7add7b4
Ehsaan Ali <38082320+EhsaanAli97@users.noreply.github.com>
2020-04-06 12:18:49 +0100

    Add files via upload
    Updated mailHandler

--- mailHandler.php
+++ mailHandler.php
@@ -1,19 +1,37 @@
 <?php
-	if(isset($_POST['submit'])){
-		$name=$_POST['name'];
-		$email=$_POST['email'];
-		$message=$_POST['message'];
+	// if(isset($_POST['submit'])){
+	// 	$name=$_POST['name'];
+	// 	$email=$_POST['email'];
+	// 	$message=$_POST['message'];
 
-		$to='ehsaanali123@gmail.com'; // Receiver Email ID, Replace with your email ID
-		$subject='Form Submission';
-		$message="Name :".$name."\n"."Wrote the following :"."\n\n".$message;
-		$headers="From: ".$email;
+	// 	$to='ehsaanali123@gmail.com'; // Receiver Email ID, Replace with your email ID
+	// 	$subject='Form Submission';
+	// 	$message="Name :".$name."\n"."Wrote the following :"."\n\n".$message;
+	// 	$headers="From: ".$email;
 
-		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-		}
-		else{
-			echo "Something went wrong!";
-		}
-	}
+	// 	if(mail($to, $subject, $message, $headers)){
+	// 		echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+	// 	}
+	// 	else{
+	// 		echo "Something went wrong!";
+	// 	}
+	// }
+
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$message=$_POST['message'];
+
+
+	$email_subject = "New Form Submission";
+
+	$email_body = "User Name: $name.\n".
+					"User Email: $email.\n".
+						"User Message: $message.\n";
+
+	$to = "ehsaanali123@gmail.com";
+
+	mail($to,$email_subject,$email_body,$headers);
+
+	header("Location: index.html");
+
 ?>
